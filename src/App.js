946012e6d3368403fa5f5d3dafbb5918429e494b
Shubham Agarwal <shubhamagarwal.alld@gmail.com>
2022-01-17 23:01:06 +0530

    router removed

--- src/App.js
+++ src/App.js
@@ -8,20 +8,24 @@ import {BrowserRouter as Router, Route, Switch,Link } from "react-router-dom";
 
 function App() {
 	return (
-        <Router>
+        // <Router>
+        //     <Header/>
+        //     <Switch>
+        //         <Route exact path='/'>
+        //             <Home/>
+        //         </Route>
+        //         <Route path='/login'>
+        //             <Login/>
+        //         </Route>
+        //         <Route path='/detail'>
+        //             <Detail/>
+        //         </Route>
+        //     </Switch>
+        // </Router>
+        <>
             <Header/>
-            <Switch>
-                <Route exact path='/'>
-                    <Home/>
-                </Route>
-                <Route path='/login'>
-                    <Login/>
-                </Route>
-                <Route path='/detail'>
-                    <Detail/>
-                </Route>
-            </Switch>
-        </Router>
+            <Home/>
+        </>
 	)
 }
 
